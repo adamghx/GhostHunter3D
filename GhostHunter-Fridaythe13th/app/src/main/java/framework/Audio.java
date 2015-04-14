d44df3645b340827c5cf4dbc97c139904a18c34b
Christian Lastova <cnl4fh@virginia.edu>
2015-04-14 18:12:38 -0400

    Audio, FileIO, Input

---
+++ GhostHunter-Fridaythe13th/app/src/main/java/framework/Audio.java
@@ -0,0 +1,10 @@
+package framework;
+
+/**
+ * Created by Christian on 4/14/2015.
+ */
+public interface Audio {
+    public Music createMusic(String file);
+
+    public Sound createSound(String file);
+}
