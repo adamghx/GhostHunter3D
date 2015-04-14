dd48970957d21dd42c8c8d49254bd6a3b1429bb1
Maurice Wong <maurice.wong24@gmail.com>
2015-04-14 18:28:52 -0400

    Implementation Classes 8,9,10,11

---
+++ GhostHunter-Fridaythe13th/app/src/main/java/framework_implementation/TouchHandler.java
@@ -0,0 +1,20 @@
+package framework_implementation;
+
+/**
+ * Created by Maurice on 4/14/2015.
+ */
+import java.util.List;
+
+import android.view.View.OnTouchListener;
+
+import framework.Input.TouchEvent;
+
+public interface TouchHandler extends OnTouchListener {
+    public boolean isTouchDown(int pointer);
+
+    public int getTouchX(int pointer);
+
+    public int getTouchY(int pointer);
+
+    public List<TouchEvent> getTouchEvents();
+}
