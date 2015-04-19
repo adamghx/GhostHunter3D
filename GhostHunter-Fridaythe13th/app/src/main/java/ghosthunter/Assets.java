064b81d6b2cd63592e669be8fd90bcdf4f6834e2
Christian Lastova <cnl4fh@virginia.edu>
2015-04-19 18:57:57 -0400

    Assets Class

---
+++ GhostHunter-Fridaythe13th/app/src/main/java/ghosthunter/Assets.java
@@ -0,0 +1,13 @@
+package ghosthunter;
+
+/**
+ * Created by Christian on 4/19/2015.
+ */
+import framework.Image;
+import framework.Sound;
+
+public class Assets {
+
+    public static Image menu;
+    public static Sound click;
+}
