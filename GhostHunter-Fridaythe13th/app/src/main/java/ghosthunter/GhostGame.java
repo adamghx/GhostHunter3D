4bf7029bcc549e8f2126a0e08e9f85a6826bbf22
Emily Zhou <emily2zhou@gmail.com>
2015-04-19 19:00:16 -0400

    LoadingScreen and GhostGame classes

--- GhostHunter-Fridaythe13th/app/src/main/java/ghosthunter/GhostGame.java
+++ GhostHunter-Fridaythe13th/app/src/main/java/ghosthunter/GhostGame.java
@@ -1,7 +1,13 @@
 package ghosthunter;
+import framework.Screen;
+import framework_implementation.AndroidGame;
 
 /**
  * Created by Emily on 4/19/2015.
  */
-public class GhostGame {
+public class GhostGame extends AndroidGame {
+    @Override
+    public Screen getInitScreen() {
+        return new LoadingScreen(this);
+    }
 }
---
+++ GhostHunter-Fridaythe13th/app/src/main/java/ghosthunter/LoadingScreen.java
@@ -0,0 +1,63 @@
+package ghosthunter;
+
+import framework.Game;
+import framework.Graphics;
+import framework.Screen;
+import framework.Graphics.ImageFormat;
+/**
+ * Created by Emily on 4/19/2015.
+ */
+public class LoadingScreen extends Screen {
+    public LoadingScreen(Game game) {
+        super(game);
+    }
+
+
+    @Override
+    public void update(float deltaTime) {
+        Graphics g = game.getGraphics();
+        Assets.menu = g.newImage("menu.jpg", ImageFormat.RGB565);
+        Assets.click = game.getAudio().createSound("explode.ogg");
+
+
+
+        game.setScreen(new MainMenuScreen(game));
+
+
+    }
+
+
+    @Override
+    public void paint(float deltaTime) {
+
+
+    }
+
+
+    @Override
+    public void pause() {
+
+
+    }
+
+
+    @Override
+    public void resume() {
+
+
+    }
+
+
+    @Override
+    public void dispose() {
+
+
+    }
+
+
+    @Override
+    public void backButton() {
+
+
+    }
+}
