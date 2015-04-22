44538ace97ae3bcdb2e39ca35bfc428b7ac64bb2
Christian Lastova <cnl4fh@virginia.edu>
2015-04-22 00:38:34 -0400

    More compatible projectile class

--- GhostHunter-Fridaythe13th/app/src/main/java/ghosthunter/Projectile.java
+++ GhostHunter-Fridaythe13th/app/src/main/java/ghosthunter/Projectile.java
@@ -11,19 +11,17 @@ public class Projectile {
     private int centerX, centerY, speed;
     private Rect projectileBox;
     private Image projectile;
+    private int savedState;
     private boolean isMovingLeft=false;
     private boolean isMovingRight=false;
     private boolean isMovingUp=false;
     private boolean isMovingDown=false;
 
-    public Projectile(boolean l, boolean r, boolean u, boolean d, int x, int y) {
+    public Projectile(int savedState, int x, int y) {
         this.centerX = x;
         this.centerY = y;
         this.projectile = Assets.projectile;
-        this.isMovingLeft = l;
-        this.isMovingRight = r;
-        this.isMovingUp = u;
-        this.isMovingDown = d;
+        this.savedState = savedState;
         this.projectileBox = new Rect(centerX, centerY,centerX + projectile.getWidth(), centerY + projectile.getHeight());
         speed = 20;
     }
@@ -31,17 +29,17 @@ public class Projectile {
     //Called in the GameScreen
     public void update(){
         projectileBox.set(this.centerX, this.centerY, centerX+projectile.getWidth(), centerY+projectile.getHeight());
-        if(this.isMovingLeft) {
-            centerX-=speed;
-        }
-        if(this.isMovingRight) {
-            centerX+=speed;
-        }
-        if(this.isMovingUp) {
+        if(this.savedState == 1 ) {
             centerY-=speed;
         }
-        if(this.isMovingDown) {
+        if(this.savedState == 2) {
+            centerX+=speed;
+        }
+        if(this.savedState == 3) {
             centerY+=speed;
+        }
+        if(this.savedState == 4) {
+            centerX-=speed;
         }
 
     }
