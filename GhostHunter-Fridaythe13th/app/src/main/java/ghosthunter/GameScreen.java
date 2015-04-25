4d202e91f9cd84c93ef889b1e991353a0d53be0b
Christian Lastova <cnl4fh@virginia.edu>
2015-04-24 23:33:53 -0400

    Ghost frequency steady increase

--- GhostHunter-Fridaythe13th/app/src/main/java/ghosthunter/GameScreen.java
+++ GhostHunter-Fridaythe13th/app/src/main/java/ghosthunter/GameScreen.java
@@ -39,6 +39,7 @@ public class GameScreen extends Screen {
     private int counter;
     int joystickMovement = 0;
     int livesLeft = 3;
+    int mod_value = 200;
     Paint paint;
 
     public GameScreen(Game game) {
@@ -151,23 +152,26 @@ public class GameScreen extends Screen {
         // 2. Check miscellaneous events like death:
 
         counter += 1;
-        if(counter % 200 == 0){
+        if(counter % mod_value == 0 && mod_value >= 50){
             int edge = (int)(Math.random()*4);
             ghosts.add(new Ghost(human, edge));
+            if(counter % 300 == 0) {
+                mod_value -= 5;
+            }
 
         }
-        if(counter >= 1000){
-            if(counter % 100 == 0){
-                int edge = (int)(Math.random()*4);
-                ghosts.add(new Ghost(human, edge));
-            }
-        }
-        if(counter >= 3000){
-            if(counter % 46 == 0){
-                int edge = (int)(Math.random()*4);
-                ghosts.add(new Ghost(human, edge));
-            }
-        }
+//        if(counter >= 1000){
+//            if(counter % 100 == 0){
+//                int edge = (int)(Math.random()*4);
+//                ghosts.add(new Ghost(human, edge));
+//            }
+//        }
+//        if(counter >= 3000){
+//            if(counter % 46 == 0){
+//                int edge = (int)(Math.random()*4);
+//                ghosts.add(new Ghost(human, edge));
+//            }
+//        }
 
 
         // 3. Call individual update() methods here.
