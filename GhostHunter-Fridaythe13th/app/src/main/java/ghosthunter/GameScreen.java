238491993409c00953613a73b7f9775605d67580
Christian Lastova <cnl4fh@virginia.edu>
2015-04-24 22:23:27 -0400

    minor change

--- GhostHunter-Fridaythe13th/app/src/main/java/ghosthunter/GameScreen.java
+++ GhostHunter-Fridaythe13th/app/src/main/java/ghosthunter/GameScreen.java
@@ -150,7 +150,7 @@ public class GameScreen extends Screen {
 
         // 2. Check miscellaneous events like death:
 
-        counter += 1;
+        counter += 50;
         if(counter % 150 == 0){
             if(ghosts.size() < 10) {
                 int edge = (int)(Math.random()*4);
